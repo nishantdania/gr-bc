8dfaa7316ac6d82a54e57222324e5fa069a9e515
nishantdania <nishantdania@gmail.com>
2017-01-19 11:10:14 +0530

    added route signup

--- app/config/routes.js
+++ app/config/routes.js
@@ -35,9 +35,7 @@ module.exports.routes = {
 	'/': {
 		view: 'homepage'
 	},
-	'/signup': {
-		view: 'homepage'
-	}
+	'POST /signup': 'UserController.create'
 
   /***************************************************************************
   *                                                                          *
